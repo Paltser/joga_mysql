2038f5edd7a173cf947e9987857415a64cf1ba72
markos <markos.paltser@voco.ee>
2023-02-01 12:59:42 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ app.engine('hbs', hbs.engine({
     layoutsDir:__dirname+ '/views/layouts/',
 }))
 
+app.use(express.static('public'));
+
 const mysql = require('mysql')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended:true}))
 
-var con = mysql.createConnection({
+let con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "qwerty",
@@ -27,7 +29,23 @@ var con = mysql.createConnection({
 con.connect(function (err){
     if (err) throw err;
     console.log('connected to joga_mysql db')
+
 })
+app.get("/", (req, res) => {
+    let query = 'SELECT * FROM article';
+    let articles = []
+    con.query(query, (err, result)=>{
+        if (err) throw err
+        articles = result
+        res.render('index', {
+            articles: articles
+        })
+    })
+});
+
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception: ', err);
+});
 
 app.listen(3000, () =>{
     console.log('app us started at http://localhost:3000');
