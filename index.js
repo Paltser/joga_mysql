e0ed1ba8b9cab875bd80aa6ffcc1e1618e0a3583
markos <markos.paltser@voco.ee>
2023-02-06 16:37:04 +0200

    Displayed author article list

--- index.js
+++ index.js
@@ -58,6 +58,19 @@ app.get('/article/:slug', (req, res) => {
         });
     });
 });
+app.get('/author/:id', (req, res) => {
+    let query = `SELECT article.name as article_name, article.image, article.slug ,author.name, article.body
+    from article JOIN author ON article.author_id = author.id where author.id = ${req.params.id}`;
+    let articles = [];
+
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result;
+        res.render('author', {
+            articles: articles
+        });
+    });
+});
 
 
 
